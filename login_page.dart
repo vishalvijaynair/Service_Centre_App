5f7b39d4f388af9b769777ea3ece2e2838dc9172
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-03-20 20:08:23 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart'; // Import the HomePage class
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:service_centre/home_page.dart';
+import 'package:service_centre/service_details.dart';
+import 'signup_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -10,121 +13,171 @@ class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-
-  // Email validation function
-  String? validateEmail(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Email is required';
-    } else if (!RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$').hasMatch(value)) {
-      return 'Enter a valid email address';
-    }
-    return null;
-  }
-
-  // Password validation function
-  String? validatePassword(String? value) {
-    if (value == null || value.isEmpty) {
-      return 'Password is required';
-    } else if (value.length < 6) {
-      return 'Password must be at least 6 characters';
-    }
-    return null;
-  }
-
-  // Function to handle successful login and navigate to HomePage
-  void _handleLoginSuccess() {
-    // Navigate to the HomePage with the desired structure
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HomePage(),
-      ),
-    );
-  }
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              'https://images.unsplash.com/photo-1569982175971-d92b01cf8694?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjJ8fGdyYWRpZW50JTIwYmFja2dyb3VuZHxlbnwwfDF8MHx8fDA%3D', // Replace with your actual image URL
-            ),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Container(
-            height: 320.0,
-            width: 320.0, // Adjust the width as needed
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 230, 205, 240),
-              borderRadius: BorderRadius.all(Radius.circular(10.0)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                      ),
-                           // Email validation
-                    ),
-                    SizedBox(height: 12.0),
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                      ),
-                      validator: validatePassword, // Password validation
-                    ),
-                    SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Check if the form is valid before proceeding
-                        if (_formKey.currentState!.validate()) {
-                          // Perform authentication logic (replace with your authentication logic)
-                          // For demonstration purposes, assuming the login is successful
-                          // You should replace this with your actual authentication logic
-                          bool loginSuccessful = true;
-
-                          if (loginSuccessful) {
-                            // Handle successful login
-                            _handleLoginSuccess();
-                          } else {
-                            // Handle unsuccessful login (show error message or perform other actions)
-                            print('Login unsuccessful');
-                          }
-                        }
-                      },
-                      child: Text('Login'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+        margin: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            _header(context),
+            _inputField(context),
+            _forgotPassword(context),
+            _signup(context),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _header(context) {
+    return const Column(
+      children: [
+        Text(
+          "Welcome",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+        Text("Enter your credential to login"),
+      ],
+    );
+  }
+
+  Widget _inputField(context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextFormField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              hintText: "Email",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: Colors.purple.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.email),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Email is required';
+              } else if (!RegExp(
+                      r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                  .hasMatch(value)) {
+                return 'Enter a valid email address';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: "Password",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: Colors.purple.withOpacity(0.1),
+              filled: true,
+              prefixIcon: const Icon(Icons.password),
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Password is required';
+              } else if (value.length < 6) {
+                return 'Password must be at least 6 characters';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () async {
+              // Validate the form
+              if (_formKey.currentState!.validate()) {
+                // If the form is valid, attempt to sign in
+                try {
+                  UserCredential userCredential =
+                      await _auth.signInWithEmailAndPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                  );
+
+                  // If login is successful, move to the home page
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomePage(),
+                    ),
+                  );
+
+                  // Get user UID for demonstration
+                  String uid = userCredential.user!.uid;
+                  print('User UID: $uid');
+                } catch (e) {
+                  // Handle login failure (incorrect credentials)
+                  print('Login failed: $e');
+                  // Show error message to user
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Incorrect Password. Please try again'),
+                    ),
+                  );
+                }
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              backgroundColor: Colors.purple.withOpacity(0.9),
+            ),
+            child: const Text(
+              "Login",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _forgotPassword(context) {
+    return TextButton(
+      onPressed: () {
+        // Implement forgot password functionality
+      },
+      child: const Text(
+        "Forgot password?",
+        style: TextStyle(color: Colors.purple),
+      ),
+    );
+  }
+
+  Widget _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? "),
+        TextButton(
+          onPressed: () {
+            // Navigate to the Sign Up Page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignupPage(),
+              ),
+            );
+          },
+          child: const Text("Sign Up", style: TextStyle(color: Colors.purple)),
+        )
+      ],
     );
   }
 }
