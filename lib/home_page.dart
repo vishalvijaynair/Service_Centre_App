ee49b20870c064800738d9b038c7396bf7bfe1ee
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-04-16 19:45:49 +0530

    Add files via upload

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,18 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:service_centre/order_details.dart';
 import 'package:service_centre/service_center_details_page.dart';
-
+import 'package:intl/intl.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         title: Text('User Requests'),
-        backgroundColor: Color.fromARGB(255, 182, 200, 247),  
+        title: Text('User Requests'),
+        backgroundColor: Color.fromARGB(255, 182, 200, 247),
         actions: [
           IconButton(
             icon: Icon(Icons.person),
@@ -25,7 +26,6 @@ class HomePage extends StatelessWidget {
         ],
       ),
       backgroundColor: Colors.white,
-      // Set background color to white
       body: StreamBuilder<QuerySnapshot>(
         stream: FirebaseFirestore.instance
             .collection('Orders_placed')
@@ -47,32 +47,127 @@ class HomePage extends StatelessWidget {
             itemCount: orders.length,
             itemBuilder: (context, index) {
               final orderData = orders[index].data() as Map<String, dynamic>;
-              // Display order details
-              return Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlue[100],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('User email: ${orders[index].id}'),
-                    SizedBox(height: 5),
-                    Text('Service: ${orderData['selectedServices'].join(', ')}'),
-                    SizedBox(height: 5),
-                    Text('Total Amount: ${orderData['totalAmount']}'),
-                    SizedBox(height: 10),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Handle button tap
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        foregroundColor:Colors.white
-                      ),
-                      child: Text('Work Done')
+              // Accessing fields from the order data
+              String userName = orderData['userName'];
+              Timestamp createdAt = orderData['createdAt'] as Timestamp;
+              String userLocation = orderData['userLocation'];
+              String userPhoneNumber = orderData['userPhoneNumber'];
+              List<dynamic> selectedServices = orderData['selectedServices'];
+              int totalAmount = orderData['totalAmount'];
+
+               // Format ordered date and time
+              DateTime orderedDateTime = createdAt.toDate();
+              String orderedDate = DateFormat.yMMMMd().format(orderedDateTime);
+              String orderedTime = DateFormat.jm().format(orderedDateTime); // Formats time with AM/PM
+
+
+            return Container(
+  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+  padding: EdgeInsets.all(10),
+  decoration: BoxDecoration(
+    color: Colors.lightBlue[100],
+    borderRadius: BorderRadius.circular(10),
+  ),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(height: 5),
+      Text(
+        'User Name:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$userName',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'Ordered Date:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$orderedDate',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'Ordered Time:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$orderedTime',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'User Location:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$userLocation',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'User Phone Number:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$userPhoneNumber',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'User Email:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '${orders[index].id}',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'Service:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '${selectedServices.join(', ')}',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        'Total Amount:',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+      ),
+      SizedBox(height: 5),
+      Text(
+        '$totalAmount',
+        style: TextStyle(fontSize: 14),
+      ),
+      SizedBox(height: 10),
+      ElevatedButton(
+        onPressed: () {
+           Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => OrderDetailsPage(
+          userEmail: orders[index].id, // Passing userEmail as userId
+        ),
+                    ),
+                  );
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 89, 40, 142),
+          foregroundColor: Colors.white,
+        ),
+        child: Text('Details'),
                     ),
                   ],
                 ),
---
+++ lib/order_details.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:service_centre/home_page.dart';
+
+class OrderDetailsPage extends StatefulWidget {
+  final String userEmail;
+    const OrderDetailsPage({Key? key, required this.userEmail}) : super(key: key);
+
+
+  @override
+  _OrderDetailsPageState createState() => _OrderDetailsPageState();
+}
+
+class _OrderDetailsPageState extends State<OrderDetailsPage> {
+  late TextEditingController _workerNameController;
+  late DateTime _selectedDate;
+  late TimeOfDay _selectedTime;
+  late TextEditingController _messageController;
+  
+  String? get userEmail => userEmail;
+
+  @override
+  void initState() {
+    super.initState();
+    _workerNameController = TextEditingController();
+    _selectedDate = DateTime.now();
+    _selectedTime = TimeOfDay.now();
+    _messageController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _workerNameController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null && pickedDate != _selectedDate) {
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? pickedTime = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+    );
+
+    if (pickedTime != null && pickedTime != _selectedTime) {
+      setState(() {
+        _selectedTime = pickedTime;
+      });
+    }
+  }
+
+  Future<void> _submitOrder() async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user = auth.currentUser;
+    if (user != null) {
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      String userId = user.uid;
+
+      // Formulate the data to be stored
+      Map<String, dynamic> orderData = {
+        'workerName': _workerNameController.text,
+        'date': DateFormat('yyyy-MM-dd').format(_selectedDate),
+        'time': _selectedTime.format(context),
+        'message': _messageController.text,
+      };
+
+      try {
+        // Store data in Firestore
+        await firestore
+            .collection('Orders_pending')
+            .doc(widget.userEmail)
+            .collection('orders')
+            .doc(user.email)
+            .set(orderData);
+
+        // Clear text fields after successful submission
+        _workerNameController.clear();
+        _messageController.clear();
+
+        // Optionally show a success message
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Details submitted successfully'),
+          ));
+          // Navigate back to the homepage
+     Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => HomePage(),
+                          ),
+                        );
+      } catch (e) {
+        print('Error submitting order: $e');
+        // Optionally show an error message
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Failed to submit Details. Please try again later.'),
+        ));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Order Details'),
+        backgroundColor: Color.fromARGB(255, 169, 170, 255),
+      ),
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.lightBlue[100],
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: _workerNameController,
+                decoration: InputDecoration(labelText: 'Worker Name'),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () => _selectDate(context),
+                child: AbsorbPointer(
+                  child: TextField(
+                    controller: TextEditingController(
+                      text: DateFormat('yyyy-MM-dd').format(_selectedDate),
+                    ),
+                    decoration: InputDecoration(
+                      labelText: 'Date',
+                      suffixIcon: Icon(Icons.calendar_today),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              GestureDetector(
+                onTap: () => _selectTime(context),
+                child: AbsorbPointer(
+                  child: TextField(
+                    controller: TextEditingController(
+                      text: _selectedTime.format(context),
+                    ),
+                    decoration: InputDecoration(
+                      labelText: 'Time',
+                      suffixIcon: Icon(Icons.access_time),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0),
+              TextField(
+                controller: _messageController,
+                decoration: InputDecoration(labelText: 'Message to User'),
+              ),
+              SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: _submitOrder,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 89, 40, 142),
+                  foregroundColor: Colors.white,
+                ),
+                child: Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
