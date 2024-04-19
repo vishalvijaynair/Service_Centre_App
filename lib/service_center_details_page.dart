53153ebda00667a6b0f878af90be63ef83cc33cc
VISHAL V NAIR <90333863+vishalvijaynair@users.noreply.github.com>
2024-04-19 11:59:53 +0530

    Add files via upload

--- lib/service_center_details_page.dart
+++ lib/service_center_details_page.dart
@@ -157,7 +157,6 @@ class UserInfoItem extends StatelessWidget {
     );
   }
 }
-
 class EditUserInfo extends StatefulWidget {
   @override
   _EditUserInfoState createState() => _EditUserInfoState();
@@ -188,135 +187,212 @@ class _EditUserInfoState extends State<EditUserInfo> {
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
       ),
       backgroundColor: Colors.white,
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 1.0,
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 55, 39, 176).withOpacity(0.1),
-            borderRadius: BorderRadius.circular(18),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-             Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: TextFormField(
-                  controller: _nameController,
-                  decoration: InputDecoration(
-                    labelText: 'Service Center Name',
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.house_rounded),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 1.0,
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 55, 39, 176).withOpacity(0.1),
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+               Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
                   ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Name is required';
-                    }
-                    return null;
-                  },
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: TextFormField(
-                  controller: _locationController,
-                  decoration: InputDecoration(
-                    labelText: 'Location(Current Location)',
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.location_on),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Location is required';
-                    }
-                    return null;
-                  },
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(18),
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    border: InputBorder.none,
-                    prefixIcon: Icon(Icons.phone),
-                  ),
-                  controller: _phoneNumberController,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Phone number is required';
-                    }
-                    if (value.length != 10 ||
-                        !RegExp(r'^[0-9]+$').hasMatch(value)) {
-                      return 'Please enter a valid 10-digit phone number';
-                    }
-                    return null;
-                  },
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                ),
-              ),
-              SizedBox(height: 16),
-              GestureDetector(
-                onTap: () async {
-                  // Get current location
-                  Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-                  // Update location controller with the obtained location
-                  setState(() {
-                    _latitude = position.latitude; // Save latitude
-                    _longitude = position.longitude; // Save longitude
-                    // Update location controller with the obtained latitude and longitude
-                    _location1Controller.text = 'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
-                  });
-                },
-                child: AbsorbPointer(
                   child: TextFormField(
-                    controller: _location1Controller,
-                    decoration: InputDecoration(labelText: 'Lat&Long(Current)', prefixIcon: Icon(Icons.location_on_rounded),),
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Service Center Name',
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.house_rounded),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Name is required';
+                      }
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
                   ),
                 ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Update user information in the database
-                  FirebaseFirestore.instance
-                    .collection('Service_Centres')
-                    .doc(FirebaseAuth.instance.currentUser!.email)
-                    .update({
-                      'Service Center Name': _nameController.text,
-                      'Phone Number': _phoneNumberController.text,
-                      'Location': _locationController.text, // Save location text separately
-                      'locValue': {
-                        'latitude': _latitude,
-                        'longitude': _longitude,
-                      },
-                    })
-                    .then((value) {
-                      Navigator.pop(context);
-                    })
-                    .catchError((error) => print('Failed to update user: $error'));
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                  backgroundColor: Colors.purple,
-                  foregroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+                SizedBox(height: 16.0),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  child: TextFormField(
+                    controller: _locationController,
+                    decoration: InputDecoration(
+                      labelText: 'Location(Current Location)',
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.location_on),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Location is required';
+                      }
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
                   ),
                 ),
-                child: Text('Save'),
-              ),
-            ],
+                SizedBox(height: 16.0),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Phone Number',
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.phone),
+                    ),
+                    controller: _phoneNumberController,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Phone number is required';
+                      }
+                      if (value.length != 10 ||
+                          !RegExp(r'^[0-9]+$').hasMatch(value)) {
+                        return 'Please enter a valid 10-digit phone number';
+                      }
+                      return null;
+                    },
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                  ),
+                ),
+                SizedBox(height: 12.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Update Services Offered List',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      for (String service in serviceAmounts.keys)
+                        Column(
+                          children: [
+                            CheckboxListTile(
+                              title: Text(service),
+                              value: selectedServices.contains(service),
+                              onChanged: (value) {
+                                setState(() {
+                                  if (value != null && value) {
+                                    selectedServices.add(service);
+                                    // If the service was not previously in the map, initialize its amount
+                                    if (!serviceAmounts.containsKey(service)) {
+                                      serviceAmounts[service] = 0;
+                                    }
+                                  } else {
+                                    selectedServices.remove(service);
+                                    // Remove the service from the map when unchecked
+                                  }
+                                });
+                              },
+                            ),
+                            SizedBox(height: 8),
+                            if (selectedServices.contains(service)) // Only show the TextFormField for selected services
+                              Container(
+                                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.purple.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(18),
+                                ),
+                                child: TextFormField(
+                                  initialValue: serviceAmounts[service].toString(), // Set initial value from the map
+                                  decoration: InputDecoration(
+                                    labelText: '$service Service Amount',
+                                    border: InputBorder.none,
+                                    prefixIcon: Icon(Icons.attach_money),
+                                  ),
+                                  keyboardType: TextInputType.number,
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Amount is required for $service';
+                                    }
+                                    return null;
+                                  },
+                                  onChanged: (value) {
+                                    setState(() {
+                                      serviceAmounts[service] = int.tryParse(value) ?? 0;
+                                    });
+                                  },
+                                ),
+                              ),
+                          ],
+                        ),
+                      SizedBox(height: 8),
+                      if (selectedServices.isEmpty) // Show the Snackbar if no service is selected
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16.0),
+                          child: Text(
+                            'Select at least one service',
+                            style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                          ),
+                        ),
+                    ],
+                  ),
+                
+
+                SizedBox(height: 16),
+                GestureDetector(
+                  onTap: () async {
+                    // Get current location
+                    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+                    // Update location controller with the obtained location
+                    setState(() {
+                      _latitude = position.latitude; // Save latitude
+                      _longitude = position.longitude; // Save longitude
+                      // Update location controller with the obtained latitude and longitude
+                      _location1Controller.text = 'Latitude: ${position.latitude}, Longitude: ${position.longitude}';
+                    });
+                  },
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      controller: _location1Controller,
+                      decoration: InputDecoration(labelText: 'Lat&Long(Current)', prefixIcon: Icon(Icons.location_on_rounded),),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Update user information in the database
+                    FirebaseFirestore.instance
+                      .collection('Service_Centres')
+                      .doc(FirebaseAuth.instance.currentUser!.email)
+                      .update({
+                        'Service Center Name': _nameController.text,
+                        'Phone Number': _phoneNumberController.text,
+                        'Location': _locationController.text, // Save location text separately
+                        'Services_offered': selectedServices,
+                          'Service_Amounts': serviceAmounts,
+                        'locValue': {
+                          'latitude': _latitude,
+                          'longitude': _longitude,
+                        },
+                      })
+                      .then((value) {
+                        Navigator.pop(context);
+                      })
+                      .catchError((error) => print('Failed to update user: $error'));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: Text('Save'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
